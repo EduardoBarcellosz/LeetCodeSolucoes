16cc6c7ee0b07f3b0ebf40858cbd14fb9730d4b3
EduardoBarcellosz <112911448+EduardoBarcellosz@users.noreply.github.com>
2025-10-28 01:17:22 -0300

    trabalhando com bits

--- 231-power-of-two/power-of-two.cs
+++ 231-power-of-two/power-of-two.cs
@@ -7,3 +7,21 @@ public class Solution {
         return false;
     }
 }
+
+
+
+// uma outra abordagem encontrada para a solução é 
+   usando o operador AND para comparar potencias de 2 pelos seus bits!
+/*
+public bool IsPowerOfTwo(int n) {
+    return n > 0 && (n & (n - 1)) == 0;
+
+1  -> 0001
+2  -> 0010
+4  -> 0100
+8  -> 1000
+
+o resultado and precisa de 1 e 1 para ativar o bit ( 1 ) se nao o resultado é 0 -> true se for 0 em bits
+
+    
+*/
